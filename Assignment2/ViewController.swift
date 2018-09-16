eaeec294e12b78603b8b5c6bf7b7d2391f1b31d1
Sydney Blackburn <sydneyblackburn10@gmail.com>
2018-09-15 19:12:05 -0700

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Sydney Blackburn on 9/15/18.
+//  Copyright © 2018 Sydney Blackburn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
